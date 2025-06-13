b48a57e80271fed9306e7737cc48a71a0586ed68
devBernardo <bernardowerty@outlook.com>
2025-06-12 22:45:31 -0300

    Finalizando pagina detalhes

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,10 +1,67 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router"
+import type { CoinProps } from "../home";
+
+
+interface ResponseData {
+    data:CoinProps;
+}
+
+
+interface ErrorData{
+    error:string;
+}
+
+type DataProps = ResponseData | ErrorData;
 
 export function Detail() {
 
+    const {cripto} = useParams();
+    const navigate = useNavigate();
+    const[coin,setCoin] = useState<CoinProps>();
+
+    useEffect(() => {
+       async function getCoin(){
+          try{
+              fetch(`https://rest.coincap.io/v3/assets/${cripto}?apiKey=406c75bc5a972308a8783b61c9c6637ceb9c4a1d04708389a883160aacb9f0fc`)
+              .then(response => response.json())
+              .then((data:DataProps) => {
+                  if("error" in data){
+                     navigate("/");
+                     return;
+                  }
+                        const price = Intl.NumberFormat("en-US",{
+                    style:"currency",
+                    currency: "USD"
+                })
+                const priceCompact = Intl.NumberFormat("en-US",{
+                    style:"currency",
+                    currency: "USD",
+                    notation: "compact"
+                })
+
+                const resultData = {
+                    ...data.data,
+                    formatedPrice: price.format(Number(data.data.priceUsd)),
+                  formatedMarket: priceCompact.format(Number(data.data.priceUsd)),
+                  formatedVolume: priceCompact.format(Number(data.data.volumeUsd24Hr))
+                }
+
+                setCoin(resultData);
+              })
+          }catch(err){
+            console.log(err);
+            navigate("/");
+          }
+       }
+
+
+       getCoin();
+    }, [cripto])
 
   return (
      <div>
-         <h1>Detail</h1>
+         <h1>{cripto}</h1>
      </div>
   )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,7 @@ import { BsSearch } from "react-icons/bs";
 import { useEffect, useState, type FormEvent } from "react";
 
 
-interface CoinProps{
+export interface CoinProps{
     id:string;
     name:string;
     symbol:string;
@@ -30,16 +30,17 @@ interface DataProp{
 export function Home() {
   const[input,setInput] = useState("");
   const[coins, setCoins] = useState<CoinProps[]>([]);
+  const [offset, setOffset] = useState(0);
 
   const navigate = useNavigate();
 
   useEffect(() => {
        getData();
-  },[])
+  },[offset]);
 
 
   async function getData(){
-      fetch("https://rest.coincap.io/v3/assets?limit=10&offset=0&apiKey=406c75bc5a972308a8783b61c9c6637ceb9c4a1d04708389a883160aacb9f0fc")
+      fetch(`https://rest.coincap.io/v3/assets?limit=10&offset=${offset}&apiKey=406c75bc5a972308a8783b61c9c6637ceb9c4a1d04708389a883160aacb9f0fc`)
       .then(response => response.json())
       .then((data:DataProp) =>{
           const coinsData = data.data;
@@ -65,7 +66,7 @@ export function Home() {
           })
 
 
-          setCoins(formatedResult);
+          setCoins([...coins, ...formatedResult]);
       })
   }
 
@@ -79,7 +80,12 @@ export function Home() {
 
 
    function handleGetMore(){
-      console.log("teste");
+      if(offset === 0){
+         setOffset(10);
+         return;
+      }
+
+      setOffset(prev => prev + 10);
    }
 
 
